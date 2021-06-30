0845cac523e1d8ac4ceb7f1d2855fbdb8eaa92bf
Aidana B <dana12021996@gmail.com>
2021-06-30 19:32:08 -0400

    adding more review

--- src/test/java/HelloAidana.java
+++ src/test/java/HelloAidana.java
@@ -2,7 +2,7 @@ public class HelloAidana {
     public static void main(String[] args) {
 
         System.out.println("Hello Aidana");
-        System.out.println("Recapping day 1 git class");
+        System.out.println("Recapping day 1 git class, T wanted to recap more");
 
     }
 }
