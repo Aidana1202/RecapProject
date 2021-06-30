c59ade50a02ae4bd935a966c2f13d08fb45acfa7
Aidana B <dana12021996@gmail.com>
2021-06-30 19:30:04 -0400

    working on recap

--- src/test/java/HelloAidana.java
+++ src/test/java/HelloAidana.java
@@ -2,6 +2,7 @@ public class HelloAidana {
     public static void main(String[] args) {
 
         System.out.println("Hello Aidana");
+        System.out.println("Recapping day 1 git class");
 
     }
 }
